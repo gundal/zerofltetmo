36049e1f2afd30bfccb00c34ff03bb667a1ef447
gundal <clauto@gmail.com>
2015-10-20 16:35:29 -0700

    ramdisk edits ready for source modifications and compile of kernel for v1-1

--- ramdisk/sbin/init.fixdeepsleep.sh
+++ ramdisk/sbin/init.fixdeepsleep.sh
@@ -1,7 +1,4 @@
 #!/system/bin/sh
 
-echo "init.fixdeepsleep.sh: start" > /dev/kmsg
+echo "init.fixdeepsleep.sh: no longer neeeded, now fixed in sources" > /dev/kmsg
 
-echo "temporary write through" > /sys/class/scsi_disk/0:0:0:1/cache_type
-echo "temporary write through" > /sys/class/scsi_disk/0:0:0:2/cache_type
-echo "temporary write through" > /sys/class/scsi_disk/0:0:0:3/cache_type
