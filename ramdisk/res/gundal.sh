7bfc772c0ebd2e5675af82c3d3c86efd0b82727e
gundal <clauto@gmail.com>
2015-10-26 13:46:06 -0700

    Version 2.1 fix backupprofile

--- ramdisk/res/gundal.sh
+++ ramdisk/res/gundal.sh
@@ -1,8 +1,6 @@
 #!/system/bin/sh
 
-#echo 400000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-#echo 1400000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-#echo 2100000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+chmod 666 -R /sys/module/lowmemorykiller/parameters/*
 
 	if [ ! -f /data/media/0/GuNDaL/profile_backup ]; then
 		echo None > /data/media/0/GuNDaL/profile_backup
