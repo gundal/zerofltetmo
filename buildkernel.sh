4d175cb8d1a996d9b52c68ce058e51a074e2ee5a
gundal <clauto@gmail.com>
2015-10-27 09:49:20 -0700

    2.2 - revert mdnie controls, fix some synapse tools

--- buildkernel.sh
+++ buildkernel.sh
@@ -40,11 +40,8 @@ find . | fakeroot cpio -H newc -o | lzop -9 > $RAMFS_TMP.cpio.lzo
 ls -lh $RAMFS_TMP.cpio.lzo
 cd $KERNELDIR
 
-echo "copying newly compiled Image to KernelDir"
-cp arch/arm64/boot/Image Image
-ls Image 
 echo "Making new boot image"
-~/bin/mkbootimg --kernel Image --dt dt.img --ramdisk $RAMFS_TMP.cpio.lzo --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o boot.img
+~/bin/mkbootimg --kernel arch/arm64/boot/Image --dt dt.img --ramdisk $RAMFS_TMP.cpio.lzo --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o boot.img
 echo -n "SEANDROIDENFORCE" >> boot.img
 if echo "$@" | grep -q "CC=\$(CROSS_COMPILE)gcc" ; then
 	dd if=/dev/zero bs=$((29360128-$(stat -c %s boot.img))) count=1 >> boot.img
